5c538437edd7077d2c55d03e552f10b01ddbb396
Diron <diron.peiris@outlook.com>
2024-07-11 01:03:02 -0500

    Update ColorVS.hlsl

--- Shaders/ColorVS.hlsl
+++ Shaders/ColorVS.hlsl
@@ -1,4 +1,36 @@
-float4 main( float4 pos : POSITION ) : SV_POSITION
+cbuffer MatrixBuffer
 {
-	return pos;
+    matrix world;
+    matrix view;
+    matrix projection;
+};
+
+struct VertexInput
+{
+    float4 position : POSITION;
+    float4 color : COLOR;
+};
+
+struct PixelInput
+{
+    float4 position : SV_POSITION;
+    float4 color : COLOR;
+};
+
+PixelInput main(VertexInput input)
+{
+    PixelInput output;
+    
+    // Set the vector's w coordinate to 1.0f for 4x4 matrix calculations.
+    input.position.w = 1.0;
+    
+    // Calculate the position of the vertex against the world, view, and projection matrices.
+    output.position = mul(input.position, world);
+    output.position = mul(output.position, view);
+    output.position = mul(output.position, projection);
+    
+    // Set the input color for the pixel shader.
+    output.color = input.color;
+    
+	return output;
 }
